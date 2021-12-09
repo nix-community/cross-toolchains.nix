1eedb30caf77d5f2750a9e15f31f83ba7d64cd2c
Jörg Thalheim <joerg@thalheim.io>
2021-12-09 17:53:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Prebuild cross-toolchains for various targets
+
+This flake provides a binary cache for various toolchains for targets `pkgsCross` for x86_64-linux.
+The cache is at https://nix-community.cachix.org
+
+Right now we only build stdenv.
---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Prebuild cross-toolchains for various targets";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }: let
+    pkgsCross = nixpkgs.legacyPackages.x86_64-linux.pkgsCross;
+  in {
+    packages.x86_64-linux = {
+      inherit (pkgsCross)
+        aarch64-multiplatform
+        armv7l-hf-multiplatform
+        riscv32
+        riscv64
+        avr
+        s390x
+        x86_64-netbsd
+        gnu32;
+    };
+    hydraJobs = nixpkgs.lib.mapAttrs (_: arch: arch.stdenv) self.packages.x86_64-linux;
+  };
+}
