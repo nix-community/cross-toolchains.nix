f80a700d6c04d46151c3f8b4214f8d16ee3f02ce
Jörg Thalheim <joerg@thalheim.io>
2021-12-10 10:20:37 +0100

    build hello for various platforms

--- flake.nix
+++ flake.nix
@@ -29,17 +29,59 @@
       "sheevaplug"
       "x86_64-netbsd"
     ];
+    clangPlatforms = [
+      "aarch64-android"
+      "aarch64-multiplatform-musl"
+      "aarch64-multiplatform"
+      "armv7l-hf-multiplatform"
+      "gnu32"
+      "m68k"
+      "mingw32"
+      "mingwW64"
+      "musl-power"
+      "musl32"
+      "musl64"
+      "or1k"
+      "pogoplug4"
+      "powernv"
+      "ppc64-musl"
+      "ppc64"
+      "ppcle-embedded"
+      "pogoplug4"
+      "powernv"
+      "ppc64-musl"
+      "ppc64"
+      "ppcle-embedded"
+      "raspberryPi"
+      "remarkable1"
+      "remarkable2"
+      "riscv64"
+      "s390"
+      "s390x"
+      "scaleway-c1"
+      "sheevaplug"
+      "wasi32"
+      "x86_64-unknown-redox"
+    ];
     stdenv-jobs = lib.mapAttrs'
-      (name: arch: lib.nameValuePair "stdenv.${name}" arch.stdenv)
+      (name: arch: lib.nameValuePair "pkgsCross.${name}.stdenv" arch.stdenv)
       self.packages.x86_64-linux;
 
+    hello-jobs = lib.mapAttrs'
+      (name: arch: lib.nameValuePair "pkgsCross.${name}.hello" arch.hello)
+      (lib.filterAttrs (n: v: !(
+        v.stdenv.targetPlatform.parsed.kernel.name != "none" || # no executable support
+        n == "x86_64-unknown-redox"                          || # no wprintf
+        true
+      )) self.packages.x86_64-linux);
+
     go-jobs = lib.mapAttrs'
-      (name: arch: lib.nameValuePair "go.${name}" arch.buildPackages.go)
+      (name: arch: lib.nameValuePair "pkgsCross.${name}.go" arch.buildPackages.go)
       (lib.getAttrs goPlatforms self.packages.x86_64-linux);
 
     clang-jobs = lib.mapAttrs'
-      (name: arch: lib.nameValuePair "clang.${name}" arch.buildPackages.clang)
-      self.packages.x86_64-linux;
+      (name: arch: lib.nameValuePair "pkgsCross.${name}.clang" arch.buildPackages.clang)
+      (lib.getAttrs clangPlatforms self.packages.x86_64-linux);
   in {
     packages.x86_64-linux = lib.filterAttrs (n: v:
       !(
@@ -52,6 +94,6 @@
         false
       )
     ) pkgsCross;
-    hydraJobs = stdenv-jobs // go-jobs // clang-jobs;
+    hydraJobs = stdenv-jobs // hello-jobs // go-jobs // clang-jobs;
   };
 }
