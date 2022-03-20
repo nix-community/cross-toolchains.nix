fa5a185abf9f3c9e8c77d88a98816893f69d984a
Jörg Thalheim <joerg@thalheim.io>
2022-03-20 21:48:03 +0100

    switch nixpkgs branch

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Prebuild cross-toolchains for various targets";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:Mic92/nixpkgs/fix-platforms";
 
   outputs = { self, nixpkgs }: let
     pkgsCross = nixpkgs.legacyPackages.x86_64-linux.pkgsCross;
@@ -87,12 +87,13 @@
   in {
     packages.x86_64-linux = lib.filterAttrs (n: v:
       !(
-        (lib.hasPrefix "iphone" n)   || # not supported on Linux
-        (lib.hasSuffix "darwin" n)   || # not supported on Linux
-        n == "amd64-netbsd"          || # deprecated alias
-        n == "x86_64-netbsd-llvm"    || # this is unfinished buisness
-        n == "fuloongminipc"         || # some definition conflict broken with glibc <-> kernelHeaders
-        n == "vc4"                   || # binutils/gcc broken; clevera wants to fix it
+        (lib.hasPrefix "iphone" n)          || # not supported on Linux
+        (lib.hasSuffix "darwin" n)          || # not supported on Linux
+        n == "amd64-netbsd"                 || # deprecated alias
+        n == "x86_64-netbsd-llvm"           || # this is unfinished buisness
+        n == "fuloongminipc"                || # some definition conflict broken with glibc <-> kernelHeaders
+        n == "vc4"                          || # binutils/gcc broken; clevera wants to fix it
+        (lib.hasPrefix "mipsisa" n)         || # junk, never worked, should be not in nixpkgs
         false
       )
     ) pkgsCross;
